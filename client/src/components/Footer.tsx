dbaa7e07f8396a3133350bf96078c5bfeb62513a
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2025-05-02 13:38:25 +0200

    ajout de l'image facebook

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,14 +1,12 @@
+import facebook from "../assets/images/facebook.png";
+
 function Footer() {
   return (
     <footer className="footer">
       <h2 className="julee-regular">© 2024 THE FANTASY WARRIORS</h2>
       <div className="imagesRS">
         <a href="https://www.facebook.com/">
-          <img
-            id="facebook"
-            src="src/assets/images/facebook.png"
-            alt="facebook"
-          />
+          <img id="facebook" src={facebook} alt="facebook" />
         </a>
         <a href="https://x.com/">
           <img id="twitter" src="src/assets/images/twitter.png" alt="twitter" />
